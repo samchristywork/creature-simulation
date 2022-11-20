ae061a30f1e07ae6de09b5d3a6ce5ad6b1856ad5
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-11-19 18:23:10 -0600

    Refactor: Added convenience for main.rs
    I've organized some config settings into variables and other minor
    changes.
    
    Package: creature-simulation@0.1.0
    Contact: Sam Christy <samchristywork@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -23,28 +23,34 @@ fn main() {
 
     let data = String::from_utf8_lossy(include_bytes!("names.in"));
     let names: Vec<&str> = data.split('\n').collect();
+    let carrying_capacity = 100;
+    let generations = 100;
+    let simulation_steps = 1000;
 
-    let mut world1 = world::World::new(80, 30, "World".to_string(), 100, false);
-    for _ in 0..100 {
+    let mut world1 = world::World::new(80, 30, "World".to_string(), carrying_capacity, false);
+    for _ in 0..carrying_capacity {
         world1.add_creature(names.choose(&mut rand::thread_rng()).unwrap());
     }
-    for generation in 0..100 {
-        let mut world2 = world::World::new(80, 30, "World".to_string(), 100, false);
+    for generation in 0..generations {
+        let mut world2 = world::World::new(80, 30, "World".to_string(), carrying_capacity, false);
         world2.add_creatures_from_world(world1);
-        world2.simulate(1000);
+        world2.simulate(simulation_steps);
         world1 = world2;
 
         info!(
-            "{} creatures survived generation {}.",
+            "{} creatures survived generation {} ({}%).",
             world1.current_state.num_alive(),
-            generation
+            generation,
+            100 * world1.current_state.num_alive() / carrying_capacity,
         );
     }
 
-    let mut world2 = world::World::new(80, 30, "World".to_string(), 100, true);
+    let mut world2 = world::World::new(80, 30, "World".to_string(), carrying_capacity, true);
     world2.add_creatures_from_world(world1);
-    world2.simulate(1000);
+    world2.simulate(simulation_steps);
     world1 = world2;
 
-    world1.display_map(DisplayMode::TerminalDynamic, &world1.history[0..], 100);
+    info!("Simulation has Ended.");
+
+    world1.display_map(DisplayMode::TerminalDynamic, &world1.history[0..], 10);
 }
